191535a2bae8c9b02922f59b0773b84c9aaec640
Dr. G Anil Kumar <aaniltumkur@gmail.com>
2026-02-11 12:17:06 +0530

    Added Program 1a starter file
    Implemented a program to perform basic arithmetic operations including addition, subtraction, multiplication, and division based on user input.

---
+++ Program1/1a_arithmetic_operations.py
@@ -0,0 +1,33 @@
+# Program to perform arithmetic operations
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("1.Addition")
+print("2.Subtraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    result = _____________
+    print("Result =", result)
+
+elif choice == 2:
+    result = _____________
+    print("Result =", result)
+
+elif choice == 3:
+    result = _____________
+    print("Result =", result)
+
+elif choice == 4:
+    if b != 0:
+        result = _____________
+        print("Result =", result)
+    else:
+        print("Division by zero not allowed")
+
+else:
+    print("Invalid choice")
